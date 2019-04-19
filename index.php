a21bc6ce31411ff20cd99bde85fdc90e4319c7b3
Jimmy Svensson <jimmy888swe@gmail.com>
2019-04-19 20:01:54 +0200

    r

--- index.php
+++ index.php
@@ -9,6 +9,44 @@
 </head>
 <body>
 <?php require_once 'process.php'; ?>
+
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$dbName = 'crud';
+$mysqli = new mysqli($host, $user, $pass, $dbName) or die(mysqli_error($mysqli));
+$result = $mysqli->query("SELECT * FROM data") or die($mysqli_error);
+#pre_r($result);
+?>
+
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Namn</th>
+                    <th>Plats</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <?php while ($row = $result->fetch_assoc()): ?>
+            <tr>
+                <td><?php echo $row['name'] ?></td>
+                <td><?php echo $row['location'] ?></td>
+                <td></td>
+            </tr>
+            <?php endwhile; ?>
+        </table>
+    </div>
+
+<?php
+function pre_r ($array){
+    echo '<pre>';
+    print_r($array);
+    echo '</pre>';
+}
+?>
     <form action="process.php" method="post">
         <label>Name: </label>
         <input type="text" name="name" value="Enter your name">
